10feb026b3bec8f0e088db4fd9cf3c6e56a90f71
Pablo Lopez Torres <pablolopeztorres@gmail.com>
2014-12-22 19:10:00 +0100

    adding phpheader to QueueConfigTest

--- Tests/Core/QueueConfigTest.php
+++ Tests/Core/QueueConfigTest.php
@@ -4,6 +4,15 @@ namespace SuperTowers\QueueBundle\Tests\Core;
 
 use \PHPUnit_Framework_TestCase;
 
+/**
+ * Unit test for QueueConfig.
+ *
+ * @category QueueBundle
+ * @package  Core
+ * @author   Pablo Lopez Torres <pablolopeztorres@gmail.com>
+ * @license  https://github.com/supertowers/queues-bundle/LICENSE.md CC-3.0
+ * @link     https://github.com/supertowers/queues-bundle
+ */
 class QueueConfigTest extends PHPUnit_Framework_TestCase
 {
     public function testExample()
