49c6b7257f2a0f7e31bbee5c8863a3d75331aa03
Pablo Lopez Torres <pablolopeztorres@gmail.com>
2014-12-22 17:52:05 +0100

    adding a first example test

---
+++ Tests/Core/QueueConfigTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SuperTowers\QueueBundle\Tests\Core;
+
+use \PHPUnit_Framework_TestCase;
+
+class QueueConfigTest extends PHPUnit_Framework_TestCase
+{
+    public function testExample()
+    {
+        $this->assertEquals(true, true);
+    }
+}
